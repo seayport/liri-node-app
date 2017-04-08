bf9e4003c6a842090cb14ff8c3347fe2c21ed1a8
Sandy Seay <seaysandra@bellsouth.net>
2017-04-08 09:17:06 -0400

    final try on bonus

--- liri.js
+++ liri.js
@@ -1,5 +1,4 @@
-
-var twitter = require('twitter');
+var  twitter  = require('twitter');
 var spotify = require('spotify');
 var request = require('request');
 // core node package for reading and writing files
@@ -7,11 +6,12 @@ var fs = require('fs');
 
 //storing the user input (command line argument) to determine action
 var userAction = process.argv[2];
+var consoleData = [getTweets.return, runSong.return, runMovie.return]
 
 
 // Twitter-Grabs the keys variables
 var keys = require("./keys.js");
-var keyslist = keys.twitterKeys
+var keyslist = keys.twitterKeys;
 
 // stores twitterKeys from the keys file.
 var client = new twitter(keys.twitterKeys);
@@ -19,25 +19,25 @@ var client = new twitter(keys.twitterKeys);
 // We will then create a switch-case statement (if-then would also work).
 // The switch-case will direct which function gets run.------------------------------------------------------
 switch (userAction) {
-  case "my-tweets":
-    getTweets();
-    break;
+    case "my-tweets":
+        getTweets();
+        break;
 
-  case "spotify-this-song":
-  //empty variable to hold song name
-    var songName = "";
-    getSong();
-    break;
+    case "spotify-this-song":
+        //empty variable to hold song name
+        var songName = "";
+        getSong();
+        break;
 
-  case "movie-this":
-  //empty variable to hold movie name
-  	var movieName = "";
-    getMovie();
-    break;
+    case "movie-this":
+        //empty variable to hold movie name
+        var movieName = "";
+        getMovie();
+        break;
 
-  case "do-what-it-says":
-    getRandom();
-    break;
+    case "do-what-it-says":
+        getRandom();
+        break;
 }
 // That is the end of actions
 
@@ -47,7 +47,7 @@ function getTweets() {
     client.get('statuses/user_timeline', { screen_name: 'TheEllenShow', count: 20 }, function(error, tweets, response) {
         if (!error) {
             for (var i = 0; i < tweets.length; i++) {
-            	console.log(tweets[i].created_at);
+                console.log(tweets[i].created_at);
                 console.log('%j \n', tweets[i].text);
             }
         }
@@ -69,13 +69,14 @@ function getSong() {
 function runSong() {
     spotify.search({ type: 'track', query: songName }, function(err, data) {
         if (err) {
-           console.log('Error occurred: ' + err);
+            console.log('Error occurred: ' + err);
             return;
         }
         console.log(data.tracks.items[0].album.artists[0].name);
         console.log(data.tracks.items[0].name);
         console.log(data.tracks.items[0].preview_url);
         console.log(data.tracks.items[0].album.name);
+        // writeLog();
     });
 } //end of spotify function-------------------------------------------------------------------------------------------
 // movie-this function------------------------------------------------------------------------------------------------
@@ -89,38 +90,49 @@ function getMovie() {
     }
 } //end of getMovie
 
-function runMovie(){
-request("http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&r=json", function(error, response, body) {
-  // If the request is successful
-  if (!error && response.statusCode === 200) {
-    // Parse the body of the site and recover just the data you request
-    console.log("Movie: " + JSON.parse(body).Title);
-    console.log("Release Year: " + JSON.parse(body).Year);
-    console.log("Rating: " + JSON.parse(body).imdbRating);
-    console.log("Production Country: " + JSON.parse(body).Country);
-    console.log("Language: " + JSON.parse(body).Language);
-    console.log("Plot: " + JSON.parse(body).Plot);
-    console.log("Actors: " + JSON.parse(body).Actors);
-    console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value);
-  		}
-	});
+function runMovie() {
+    request("http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&r=json", function(error, response, body) {
+        // If the request is successful
+        if (!error && response.statusCode === 200) {
+            // Parse the body of the site and recover just the data you specify
+            console.log("Movie: " + JSON.parse(body).Title);
+            console.log("Release Year: " + JSON.parse(body).Year);
+            console.log("Rating: " + JSON.parse(body).imdbRating);
+            console.log("Production Country: " + JSON.parse(body).Country);
+            console.log("Language: " + JSON.parse(body).Language);
+            console.log("Plot: " + JSON.parse(body).Plot);
+            console.log("Actors: " + JSON.parse(body).Actors);
+            console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value);
+            // writeLog();
+            // } trying to deal with no rotten tomatoes rating
+            // else (response.statusCode === undefined){
+            // console.log("Sorry, information not found!");
+        }
+
+    });
 }
 
 function getRandom() {
-fs.readFile("random.txt", "utf8", function(error, data) {
-	var randomArr = data.split(",")
-	if (randomArr[0] === "spotify-this-song") {
-	songName = randomArr[1];
-	runSong();
-	} if (randomArr[0] === "movie-this") {
-	movieName = randomArr[1];
-	runMovie();
-	}
-	else {
-	randomArr[0] === "my-tweets";
-	getTweets();
-	}
-	
-  })//end of readFile 
+    fs.readFile("random.txt", "utf8", function(error, data) {
+            var randomArr = data.split(",")
+            if (randomArr[0] === "spotify-this-song") {
+                songName = randomArr[1];
+                runSong();
+            }
+            if (randomArr[0] === "movie-this") {
+                movieName = randomArr[1];
+                runMovie();
+            } else {
+                randomArr[0] === "my-tweets";
+                getTweets();
+            } 
 
-}//end of getDoit
+        }) //end of readFile 
+
+} //end of getRandom
+// Bonus code not complete
+// function writeLog() {
+//     fs.appendFile("log.txt", JSON.stringify(log.txt)) 
+//                     } 
+
+     
